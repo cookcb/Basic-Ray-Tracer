7b2859e1e108c1bb3fe2088275efb61a4230a0c7
Christian <cbcook321@gmail.com>
2016-05-08 23:19:52 -0400

    Debugging Anti_Aliasing.java
    Now presents the correct image

--- src/Assignment1/Anti_Aliasing.java
+++ src/Assignment1/Anti_Aliasing.java
@@ -19,10 +19,12 @@ public class Anti_Aliasing extends JFrame implements GLEventListener{
 	final int height = 512;
 
 	Buffer scene;
-	
-	public final static int  dimension = 512;
-	public float l = -0.1f, r = 0.1f, b = -0.1f, t = 0.1f, d = 0.1f, A, B, C, A2, B2, C2, A3, B3, C3;
-	public Spheres green, blue, red;
+	Vectors lightSource = new Vectors(-4, 4, -3);	
+	Spheres green = new Spheres(0, 0, -7, 2, 32.0f, new Vectors(0.0f, 0.2f, 0.0f), new Vectors(0.0f, 0.5f, 0.0f), new Vectors(0.5f, 0.5f, 0.5f));
+	Spheres blue = new Spheres(-4 , 0, -7, 1, 0, new Vectors(0.2f, 0, 0), new Vectors(1, 0, 0), new Vectors(0, 0, 0));
+	Spheres red = new Spheres(4, 0, -7, 1, 0, new Vectors(0, 0, 0.2f), new Vectors(0, 0, 1), new Vectors(0, 0, 0));
+	Plane plane = new Plane(new Vectors(0, 1 ,0), 0, new Vectors(0.2f, 0.2f, 0.2f), new Vectors(1f, 1f, 1f), new Vectors(0f, 0f, 0f), new Vectors(0, -2, 0));
+	Surfaces[] objects = {green, red, blue, plane};
 
 	public Anti_Aliasing() {
 
@@ -47,19 +49,13 @@ public class Anti_Aliasing extends JFrame implements GLEventListener{
 	}
 
 	public Buffer renderScene() {
-		Vectors u, v, w, e;
-		Vectors light = new Vectors(-4, 4, -3);
+		float d = 0.1f;
 		float U, V;
 		float[] pixelValues = new float[width * height * 3];
+		//eye point
+		Vectors e = new Vectors(0, 0, 0);
 		int count = 64;
-		u = new Vectors(1, 0, 0);
-		v = new Vectors(0, 1, 0);
-		w = new Vectors(0, 0, 1);
-		e = new Vectors(0, 0, 0);
-		green = new Spheres(0, 0, -7, 2, 32.0f, new Vectors(0, 0, 0), new Vectors(0, 0, 0), new Vectors(0, 0, 0));
-		blue = new Spheres(4, 0, -7, 1, 0, new Vectors(0, 0, 0), new Vectors(0, 0, 0), new Vectors(0, 0, 0));
-		red = new Spheres(-4, 0, -7, 1, 0, new Vectors(0, 0, 0), new Vectors(0, 0, 0), new Vectors(0, 0, 0));
-		
+
 
 		for(int q = 0; q < count; q++){
 			Random rand = new Random();
@@ -89,231 +85,87 @@ public class Anti_Aliasing extends JFrame implements GLEventListener{
 
 					D = D.unitV();
 
-					float disc1 = green.intersect(D, e);
-					float disc2 = blue.intersect(D, e);
-					float disc3 = red.intersect(D, e);
-
-					if(disc1 > 0){            		
-						float t = green.getT(D, e);              		
-						float max, max2; 
-
-						Vectors intsecP = D.ScalarMult(t);   
-
-
-						Vectors View = new Vectors(e.getX() - intsecP.getX(), e.getY() - intsecP.getY(), e.getZ() - intsecP.getZ());
-						View = View.unitV();
-						Vectors l = light.sub(intsecP);
-
-						l = l.unitV();
-
-
-						Vectors n = new Vectors((intsecP.getX() - green.getX()), 
-								(intsecP.getY() - green.getY()), 
-								(intsecP.getZ() - green.getZ()));              		
-						n = n.unitV();       
-
-						Vectors Ka = new Vectors(0.0f, 0.2f, 0.0f);
-						Vectors Kd = new Vectors(0.0f, 0.5f, 0.0f);
-						Vectors Ks = new Vectors(0.5f, 0.5f, 0.5f);   
-
-						if(n.dot(l) < 0){            			
-							max = 0.0f;
-						}else{
-							max = (float) n.dot(l);            			
-						} 
-
-						Vectors Ld = new Vectors(Kd.getX() * max, Kd.getY() * max, Kd.getZ() * max);
-
-						Vectors h = new Vectors(View.getX() + l.getX(), View.getY() + l.getY(), View.getZ() + l.getZ());
-
-						h = h.unitV();            		            		
-						if(n.dot(h) < 0){
-							max2 = 0;
-						}else{
-							max2 = (float) n.dot(h);
-						}
-						Vectors L;
-						if(red.intersect(l, intsecP) > 0){
-							L = Ka;
-						}else{       
-
-							Vectors Ls = Ks.ScalarMult((float) Math.pow(max2, 32));              		
-							Vectors La = Ka;            		
-							L = Ld;
-							L = L.add(La);
-							L = L.add(Ls);
-						}
-
-
-
-
-						pixelValues[i + 0] = pixelValues[i + 0] + (float) Math.pow(L.getX(), 1.0 /2.2f); // red
-						pixelValues[i + 1] = pixelValues[i + 1] + (float) Math.pow(L.getY(), 1.0 /2.2f); // green
-						pixelValues[i + 2] = pixelValues[i + 2] + (float) Math.pow(L.getZ(), 1.0 /2.2f); // blue
-					}else if(disc2 > 0){
-						float t = blue.getT(D, e);              		
-						float SP = 0.0f, max, max2; 
-
-						Vectors intsecP = D.ScalarMult(t);   
-						Vectors View = new Vectors(intsecP.getX() * -1f, intsecP.getY() * -1f, intsecP.getZ() * -1f);
-						Vectors l = light.sub(intsecP);
-						l = l.unitV(); 
-
-
-						Vectors n = new Vectors((intsecP.getX() - blue.getX()) / blue.getRadius(), 
-								(intsecP.getY() - blue.getY()) / blue.getRadius(), 
-								(intsecP.getZ() - blue.getZ()) / blue.getRadius());              		
-						n = n.unitV();       
-
-						Vectors Ka = new Vectors(0, 0, 0.2f);
-						Vectors Kd = new Vectors(0, 0, 1);
-						Vectors Ks = new Vectors(0, 0, 0);            		
-						if(n.dot(l) < 0){            			
-							max = 0.0f;
-						}else{
-							max = (float) n.dot(l);            			
-						}            		
-						Vectors Ld = Kd.ScalarMult(max);      		
-
-						Vectors h = View.add(l);
-						h = h.unitV();            		            		
-						if(n.dot(h) < 0){
-							max2 = 0;
-						}else{
-							max2 = (float) n.dot(h);
-						}
-						Vectors L;
-						if(red.intersect(l, intsecP) > 0 || green.intersect(l, intsecP) > 0){
-							L = Ka;
-						}else{
-
-							Vectors Ls = Ks.ScalarMult((float) Math.pow(max2, SP));              		
-							Vectors La = Ka;            		
-							L = La.add(Ld);
-							L = L.add(Ls);
-						}
-
-
-
-						pixelValues[i + 0] = pixelValues[i + 0] + (float) Math.pow(L.getX(), 1.0 /2.2f); // red
-						pixelValues[i + 1] = pixelValues[i + 1] + (float) Math.pow(L.getY(), 1.0 /2.2f); // green
-						pixelValues[i + 2] = pixelValues[i + 2] + (float) Math.pow(L.getZ(), 1.0 /2.2f); // blue
-					}else if(disc3 > 0){
-						float t = red.getT(D, e);              		
-						float SP = 0.0f, max, max2; 
-
-						Vectors intsecP = D.ScalarMult(t);           		            		 
-						Vectors l = light.sub(intsecP);
-						l = l.unitV(); 
-
-						Vectors n = new Vectors((intsecP.getX() - red.getX()) / red.getRadius(), 
-								(intsecP.getY() - red.getY()) / red.getRadius(), 
-								(intsecP.getZ() - red.getZ()) / red.getRadius());              		
-						n = n.unitV();       
-
-						Vectors Ka = new Vectors(0.2f, 0, 0);
-						Vectors Kd = new Vectors(1, 0, 0);
-						Vectors Ks = new Vectors(0, 0, 0);           		
-						if(n.dot(l) < 0){            			
-							max = 0.0f;
-						}else{
-							max = (float) n.dot(l);            			
-						}            		
-						Vectors Ld = Kd.ScalarMult(max);      		
-						Vectors h = D.add(l);
-
-						h = h.unitV();            		            		
-						if(n.dot(h) < 0){
-							max2 = 0;
-						}else{
-							max2 = (float) n.dot(h);
-						}
-						Vectors Ls = Ks.ScalarMult((float) Math.pow(max2, SP));  
-
-						Vectors La = Ka;            		
-						Vectors L = La.add(Ld);
-						L = L.add(Ls); 
-
-
-
-						pixelValues[i + 0] = pixelValues[i + 0] + (float) Math.pow(L.getX(), 1.0 /2.2f); // red
-						pixelValues[i + 1] = pixelValues[i + 1] + (float) Math.pow(L.getY(), 1.0 /2.2f); // green
-						pixelValues[i + 2] = pixelValues[i + 2] + (float) Math.pow(L.getZ(), 1.0 /2.2f); // blue
-					}else{
-						Vectors Ka = new Vectors(0.2f, 0.2f, 0.2f);
-						Vectors Kd = new Vectors(1f, 1f, 1f);
-						Vectors Ks = new Vectors(0f, 0f, 0f); 
-
-						Vectors normal = new Vectors(0, 1 ,0);
-						normal = normal.unitV();
-						Vectors pointOnPlane = new Vectors(0, -2, 0);
-						pointOnPlane = pointOnPlane.unitV();
-
-						float T = -2 / D.getY();
-
-						if(T >= 0){
-
-							float max, max2;
-
-							Vectors intsecP = new Vectors(D.getX() * T, D.getY() * T, D.getZ() * T);
-
-							Vectors l = light.sub(intsecP);
-							l = l.unitV();
-							if(normal.dot(l) < 0){            			
-								max = 0.0f;
-							}else{
-								max = (float) normal.dot(l);            			
-							}            		
-							Vectors Ld = Kd.ScalarMult(max);  
-							Vectors V = intsecP.invert();
-							V.unitV();
-							Vectors h = intsecP.invert().add(l);
-
-							h = h.unitV();            		            		
-							if(normal.dot(h) < 0){
-								max2 = 0;
-							}else{
-								max2 = (float) normal.dot(h);
-							}
-							Vectors L;
-
-							if(red.intersect(l, intsecP) > 0 || green.intersect(l, intsecP) > 0 || blue.intersect(l, intsecP) > 0){
-
-								L = Ka;
-							}else{
-
-								Vectors Ls = Ks.ScalarMult((float) Math.pow(max2, 0));              		
-								Vectors La = Ka;            		
-								L = La.add(Ld);
-								L = L.add(Ls);
-							}
-							pixelValues[i + 0] = pixelValues[i + 0] + (float) Math.pow(L.getX(), 1.0 /2.2f); // red
-							pixelValues[i + 1] = pixelValues[i + 1] + (float) Math.pow(L.getY(), 1.0 /2.2f); // green
-							pixelValues[i + 2] = pixelValues[i + 2] + (float) Math.pow(L.getZ(), 1.0 /2.2f); // blue
-						}else{
-
-							pixelValues[i + 0] = pixelValues[i + 0] + 0; // red
-							pixelValues[i + 1] = pixelValues[i + 1] + 0; // green
-							pixelValues[i + 2] = pixelValues[i + 2] + 0; // blue
+					float closest = Integer.MAX_VALUE;
+					Surfaces surf = null;
+					//Calculate the closest object the viewing ray hits
+					for(int k = 0; k < objects.length; k++){
+						float T = objects[k].getT(D, e);
+						if(T >= 0 && T <= closest){
+							closest = T;
+							surf = objects[k];
 						}
 					}
+					if(surf == null){
 
+					}else{
+						Vectors intsecP;
+						//The point at which the object is hit
+						intsecP = D.ScalarMult(surf.getT(D, e));
+						intsecP = intsecP.add(e);
+						//Vector coming from light source
+						Vectors light = lightSource.sub(intsecP);
+						light = light.unitV();
+						Vectors norm = surf.getNormal(intsecP);
+						norm = norm.unitV();
+						//Ray going from eye position to the hit object using normalized direction
+						Ray eyeRay;
+						eyeRay = new Ray(D.invert(), e);
+						eyeRay.normalizeD();
 
+						//Calculation of blin-phong shadeing with specular, diffuse and ambient
+						Vectors shadeing = Phong(surf, eyeRay, norm, intsecP, light, surf.getKA(), surf.getKD(), surf.getKS(), surf.getSpecular());
+
+						//Add each color value with gamma of 2.2
+						pixelValues[i + 0] = pixelValues[i + 0] + (float) Math.pow(shadeing.getX(), 1.0 /2.2f); // red
+						pixelValues[i + 1] = pixelValues[i + 1] + (float) Math.pow(shadeing.getY(), 1.0 /2.2f); // green
+						pixelValues[i + 2] = pixelValues[i + 2] + (float) Math.pow(shadeing.getZ(), 1.0 /2.2f); // blue
+					}
 				}            	
 			}
-
 		}
+		//To average out the pixel values for anti-aliasing
 		for(int sandra = 0; sandra < width * height * 3; sandra++){
 			pixelValues[sandra] = pixelValues[sandra] / 64;
 		}
 		return FloatBuffer.wrap(pixelValues);
 
 	}
-	
+	public Vectors Phong(Surfaces object, Ray eyeRay, Vectors normal, Vectors intsecP, Vectors light, Vectors ambient, Vectors diffuse, Vectors specular, float SP){
+		Vectors phong = new Vectors(0, 0, 0);
+		boolean shadows = false;
+
+		//Checks to see if the pixel is in shadow
+		for(int i = 0; i < objects.length; i++){
+			if(object == objects[i]){
+
+			}else if(objects[i].getT(light, intsecP) > 0){
+				shadows = true;
+			}
+		}
+
+		if(shadows == true){
+			//Only apply ambient if pixel is in shadow
+			phong = ambient;
+		}else{
+			float max, max2;
+			//Calculations for diffuse shadeing
+			max = (float) Math.max(0, normal.dot(light));
+			Vectors Ld = diffuse.ScalarMult(max);      		
+
+			//Calculations for specular highlight
+			Vectors h = eyeRay.getDirection().add(light).unitV();           		            		
+			max2 = (float) Math.max(0, normal.dot(h));
+			Vectors Ls = specular.ScalarMult((float) Math.pow(max2, object.getSpecular()));           		
+
+			//Combining all three shadeing parameters
+			phong = ambient.add(Ld).add(Ls);
+		}
+		return phong;
+	}
 	public void display(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2();
 		gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
-		
+
 		gl.glDrawPixels(width, height, GL2.GL_RGB, GL2.GL_FLOAT, this.scene);
 
 		gl.glFlush();
@@ -331,7 +183,7 @@ public class Anti_Aliasing extends JFrame implements GLEventListener{
 			int arg4) {
 
 	}
-	
+
 	public static void main(String[] args) {
 		Anti_Aliasing AA = new Anti_Aliasing();
 	}
